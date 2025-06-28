1dc56158817e20f8b86c318c1852ea61133c0ede
saravananCodeExplorer <saravanansekar434@gmail.com>
2025-06-28 09:43:31 +0530

    sidebar removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Sidebar from "./components/Sidebar";
 import ChatArea from "./components/ChatArea";
 import Directory from "./components/Directory";
 import Navbar from "./components/NavBar";
@@ -10,7 +9,6 @@ const App: React.FC = () => {
     <div className="flex h-screen bg-gray-100 text-gray-800">
       <Navbar/>
       <MessageList/>
-      {/* <Sidebar /> */}
       <ChatArea />
       <Directory />
     </div>
